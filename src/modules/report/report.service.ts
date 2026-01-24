177a5d5ddaf7e08dc922ed7e597255053a809bfb
Nattaporn Detdee <nattaporn.d@hoteliers.guru>
2026-01-24 14:46:10 +0700

    feat: check data in redis, if dont exist, then sync data from google sheet

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -13,7 +13,6 @@ import {
 @Injectable()
 export class ReportService {
   private readonly logger = new Logger(ReportService.name);
-  private readonly cacheKey = 'REPORT_PRIMARY_DATA';
   private readonly spreadsheetId =
     '1dm58io1Hk0iRKu7rTcYimmQ5LZHZJF5YulN0lQ1jlcY';
   private readonly range = 'REPORT!A:D';
@@ -28,6 +27,8 @@ export class ReportService {
    * Get all data in the system (Types -> Years -> Months)
    */
   async getAllReportData(): Promise<AllReportData> {
+    await this.checkAndSyncData();
+
     const types = await this.redis.smembers('report:types');
     const result: AllReportData = {};
 
@@ -49,6 +50,8 @@ export class ReportService {
    * Fetch all available years for a specific type
    */
   async getAllYearsReport(type: ReportType) {
+    await this.checkAndSyncData();
+
     const yearsKey = `report:${type}:years`;
     // Get year list from Set
     const availableYears = await this.redis.smembers(yearsKey);
@@ -67,6 +70,8 @@ export class ReportService {
    * Fetch data for a specific year (12 months)
    */
   async getAnnualReport(type: ReportType, year: string) {
+    await this.checkAndSyncData();
+
     const key = `report:${type}:${year}`;
     const data = await this.redis.hgetall(key);
     return this.formatFullYear(data);
@@ -81,6 +86,8 @@ export class ReportService {
     start: number,
     end: number,
   ) {
+    await this.checkAndSyncData();
+
     const key = `report:${type}:${year}`;
     const fields = Array.from({ length: end - start + 1 }, (_, i) =>
       (start + i).toString().padStart(2, '0'),
@@ -97,17 +104,19 @@ export class ReportService {
    * Sync data and update the Index Set
    */
   async syncFromSheets() {
-    const raw = await this.googleSheets.getSheetValues(
+    this.logger.log('Starting data sync from Google Sheets...');
+
+    const rawRows = await this.googleSheets.getSheetValues(
       this.spreadsheetId,
       this.range,
     );
 
-    const reports = ReportDomain.transformRawToReports(raw);
+    const reports = ReportDomain.transformRawToReports(rawRows);
 
     // Inspect reports before Redis sync
-    // View first 15 rows in table
-    console.log('--- Debug reports ---');
-    console.table(reports.slice(0, 15));
+    // View first 20 rows in table
+    // console.log('--- Debug reports ---');
+    // console.table(reports.slice(0, 20));
 
     for (const type of REPORT_TYPES) {
       // Store Master Index for types
@@ -130,6 +139,16 @@ export class ReportService {
       // Store Year Index
       await this.redis.sadd(`report:revenue:years`, yearStr);
       await this.redis.sadd(`report:hotels:years`, yearStr);
+    }
+  }
+
+  private async checkAndSyncData(): Promise<void> {
+    const types = await this.redis.smembers('report:types');
+    if (types.length === 0) {
+      this.logger.log(
+        'Cache is empty. Triggering data sync from Google Sheets...',
+      );
+      await this.syncFromSheets();
     }
   }
 
